0af492972ef8aa017f4303a96b6661e4857a5dc8
Prysiazhnyi Daniil <prysiazhnyi.d@gmail.com>
2022-09-19 15:04:39 +0300

    setted api

---
+++ src/API/types.tsx
@@ -0,0 +1,47 @@
+export interface GetAllAccountsResDto extends Array<Account> {}
+
+export interface AddAccountReqDto extends Account {}
+export interface AddAccountResDto extends Account {
+    disabled: boolean
+}
+export interface UpdateAccountReqDto extends Account {}
+export interface UpdateAccountResDto extends Account {
+    disabled: boolean
+}
+
+export interface SwitchAccountReqDto {
+    disable: boolean,
+    username: string
+}
+
+export interface GetProxiesResDto extends Array<SimpleProxy> {}
+
+export interface SimpleProxy {
+    label: string
+}
+
+export interface Account {
+    _id: string
+    password: string
+    proxies: string[]
+    cookies: Cookies
+    userAgent: string
+    username: string
+  }
+  
+export interface Cookies {
+    [key: string]: Cookie[]
+}
+
+export interface Cookie {
+    name: string
+    value: string
+    path?: string
+    expires?: string
+    secure?: boolean
+    httpOnly?: boolean
+}
+
+export interface GenericErrorResDto {
+    message: string
+}
