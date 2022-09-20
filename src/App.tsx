37ddd9a3285d1f3b7cbeb715e745645e750551db
Prysiazhnyi Daniil <prysiazhnyi.d@gmail.com>
2022-09-20 14:00:50 +0300

    added modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Modal } from './components/common/Modal/Modal.component';
 import { AccountsTable } from './components/features/AccountsTable/AccountsTable.components';
 import { Container } from './components/layout/Container/Container.component';
 
---
+++ src/components/common/Modal/Modal.component.tsx
@@ -0,0 +1,20 @@
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom';
+import { ModalProps } from './Modal.props'
+
+export const Modal = ({isOpen = false, content}: ModalProps) => {
+
+  return isOpen ? ReactDOM.createPortal(
+(<div className="relative z-10" aria-labelledby="modal-title" role="dialog" aria-modal="true">
+  <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"></div>
+
+  <div className="fixed inset-0 z-10 overflow-y-auto">
+    <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+      <div className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
+        {content}
+      </div>
+    </div>
+  </div>
+</div>)
+  , document.getElementById("modal")!) : null;
+}
